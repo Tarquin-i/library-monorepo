e10f0ff49b0105cf56536acf9dcd7816d48bee25
Tarquin <3442322864@qq.com>
2026-04-10 17:44:35 +0800

    chore(auth):调整 better-auth 会话缓存配置

--- apps/server/lib/auth.ts
+++ apps/server/lib/auth.ts
@@ -10,6 +10,7 @@ export const auth = betterAuth({
   }),
   emailAndPassword: {
     enabled: true,
+    maxAge: 5 * 60, // 缓存 5 分钟，getSession 先走 Cookie 里面的缓存
   },
   // 在 better-auth 基础上加新字段，同步告知加了什么字段（不然里面只会用默认那几个字段
   user: {
@@ -19,7 +20,7 @@ export const auth = betterAuth({
         defaultValue: 'reader',
         // 不让用户注册的时候自己传值
         input: false,
-      }
-    }
-  }
+      },
+    },
+  },
 });
