a85ae40c3197f6eebedf1baacdbfa20a00c18d61
Tarquin <3442322864@qq.com>
2026-04-15 22:01:32 +0800

    refactor(auth):收紧页面访问权限配置

--- apps/client/src/routes/access-control.tsx
+++ apps/client/src/routes/access-control.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute('/access-control')({
     // const session = await authClient.getSession();
     // console.log(session.data?.user?.role);
     // 路由守卫，传递当前页面所需的角色类型，检查用户是否具有访问权限
-    await roleCheck(['admin', 'librarian', 'reader']);
+    await roleCheck(['admin']);
   },
   component: RouteComponent,
 });
