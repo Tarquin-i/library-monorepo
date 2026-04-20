826e9bfc33f1232f2cbc6886fc759dd9f047a896
Tarquin <3442322864@qq.com>
2026-04-20 18:06:09 +0800

    feat(deploy):接入阿里云FC3部署适配

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -4,21 +4,32 @@ import { auth } from './lib/auth';
 import { initCors } from './lib/cors';
 import { logger } from './lib/logger';
 import { requireAuth } from './lib/permission';
+import { handle } from 'hono-alibaba-cloud-fc3-adapter';
 
-const app = new Hono();
+function createApp() {
+  const app = new Hono();
 
-// CORS
-initCors(app);
+  // CORS
+  initCors(app);
 
-app.use('*', logger);
+  app.use('*', logger);
 
-app.use('/api/v1/*', requireAuth);
+  app.use('/api/v1/*', requireAuth);
 
-// 业务路由
-const routes = app.route('/api', api);
+  // 业务路由
+  const routes = app.route('/api', api);
 
-// better-auth 认证
-app.on(['POST', 'GET'], '/api/auth/*', (c) => auth.handler(c.req.raw));
+  // better-auth 认证
 
-export type AppType = typeof routes;
+  const appWithAuth = routes.on(['POST', 'GET'], '/api/auth/*', (c) =>
+    auth.handler(c.req.raw),
+  );
+  return appWithAuth;
+}
+
+const app = createApp();
+
+export type AppType = typeof app;
+export const handler = handle(app);
+
 export default app;
