b5ac8fd654f03772f8c5bc97a63a91dd1fbed9c4
Tarquin <3442322864@qq.com>
2026-04-23 00:13:29 +0800

    fix(db):补全认证表更新时间默认值
    - 为 session.updatedAt 增加 defaultNow 配置
    - 为 account.updatedAt 增加 defaultNow 配置，避免插入新记录时缺失默认值

--- packages/db/schema/user.entity.ts
+++ packages/db/schema/user.entity.ts
@@ -25,6 +25,7 @@ export const session = pgTable(
     token: text('token').notNull().unique(),
     createdAt: timestamp('created_at').defaultNow().notNull(),
     updatedAt: timestamp('updated_at')
+      .defaultNow()
       .$onUpdate(() => /* @__PURE__ */ new Date())
       .notNull(),
     ipAddress: text('ip_address'),
@@ -54,6 +55,7 @@ export const account = pgTable(
     password: text('password'),
     createdAt: timestamp('created_at').defaultNow().notNull(),
     updatedAt: timestamp('updated_at')
+      .defaultNow()
       .$onUpdate(() => /* @__PURE__ */ new Date())
       .notNull(),
   },
