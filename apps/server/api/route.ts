6e4425c08fe5039799a80e1a4f3e50f01ec0a8cb
Tarquin <3442322864@qq.com>
2026-04-27 16:17:42 +0800

    refactor(api):移除自定义用户角色管理接口
    - 删除服务端用户列表与角色修改 Hono 路由
    
    - 从 API 聚合路由中移除用户接口注册
    
    - 删除空 open-route 文件

--- apps/server/api/route.ts
+++ apps/server/api/route.ts
@@ -2,10 +2,8 @@ import { Hono } from 'hono';
 import bookApp from './v1/book.handle';
 import borrowingApp from './v1/borrowing.handle';
 import renewalApp from './v1/renewal.handle';
-import userApp from './v1/user.handle';
 
 const api = new Hono()
-  .route('/v1', userApp)
   .route('/v1', bookApp)
   .route('/v1', borrowingApp)
   .route('/v1', renewalApp);
